da0999aaacbebefeeff06ee82f2dd71f490630cf
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-22 06:37:34 -0400

    Dealership - DTO (Automobile) [22/10/2023]

---
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/DealershipJpaMavenDesktopJava.java
@@ -0,0 +1,32 @@
+package com.halconbit.dealership.jpa.maven.desktop.java;
+
+/**
+ * @author Brayan Esteves
+ * Challenge:
+ * [ENGLISH]:
+ * Monthly Challenge No1. A car sales store needs a system that allows them to
+ * perform a CRUD of all the vehicles they currently have for sale to then publish
+ * them in a catalog.
+ * Perform the corresponding modeling of the Automobile class, taking into account
+ * the following data: id, model, brand, engine, color, license(Plate), numberOfDoors.
+ * Implement the necessary methods to allow CRUD operations of each car.
+ * IMPORTANT: Respect the layer model, separating the responsibility of each of
+ * them: logic, graphical interface (If implemented) and persistence.
+ * [=======]
+ * [SPANISH]:
+ * Desafío Mensual Nº1.
+ * Una tienda de venta de automóviles necesita de un sistema que le permita realizar
+ * un CRUD de todos los vehículos que tiene actualmente a la venta para luego
+ * publicarlos en un catálogo.
+ * Realizar el modelado correspondiente de la clase Automovil, teniendo en centa
+ * los siguientes datos: id, modelo, marca, motor, color, patente(placa), cantidadPuertas.
+ * Realizar los métodos necesarios para permitir las operaciones CRUD de cada automóvil.
+ * IMPORTANTE: Respetar el modelo de capas, separando la responsabilidad de cada
+ * una de ellas: lógica, interfaz gráfica (En caso de realizarla) y persistencia.
+ */
+public class DealershipJpaMavenDesktopJava {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/logic/Automobile.java
@@ -0,0 +1,84 @@
+package com.halconbit.dealership.jpa.maven.desktop.java.logic;
+
+public class Automobile {
+    
+    private int    id;
+    private String model;
+    private String brand;
+    private String engine;
+    private String color;
+    private String license;
+    private int    numberOfDoors;
+
+    public Automobile() {
+    }
+
+    public Automobile(int id, String model, String brand, String engine, String color, String license, int numberOfDoors) {
+        this.id            = id;
+        this.model         = model;
+        this.brand         = brand;
+        this.engine        = engine;
+        this.color         = color;
+        this.license       = license;
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getEngine() {
+        return this.engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLicense() {
+        return this.license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public int getNumberOfDoors() {
+        return this.numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+    
+    
+    
+}
