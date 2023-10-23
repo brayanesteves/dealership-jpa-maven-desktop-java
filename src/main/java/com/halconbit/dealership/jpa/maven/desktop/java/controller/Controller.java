65096302e0c9a4aae220efa1f69cc6c1dea134a2
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-23 11:55:26 -0400

    Persistence - Controller Persistence [23/10/2023]

--- src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/controller/Controller.java
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/controller/Controller.java
@@ -1,9 +1,27 @@
 package com.halconbit.dealership.jpa.maven.desktop.java.controller;
 
+import com.halconbit.dealership.jpa.maven.desktop.java.logic.Automobile;
+import com.halconbit.dealership.jpa.maven.desktop.java.persistence.Persistence;
+
 /**
  *
  * @author Brayan Esteves
  */
 public class Controller {
+    
+    Persistence persistence = new Persistence();
+    
+    public void AddAutomobile(String model, String brand, String engine, String color, String license, int numberOfDoors) {
+        
+        Automobile automobile = new Automobile();
+        automobile.setModel(model);
+        automobile.setBrand(brand);
+        automobile.setEngine(engine);
+        automobile.setColor(color);
+        automobile.setLicense(license);
+        automobile.setNumberOfDoors(numberOfDoors);
+        
+        this.persistence.addAutomobile(automobile);
+    }
     
 }
--- src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/persistence/Persistence.java
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/persistence/Persistence.java
@@ -1,9 +1,15 @@
 package com.halconbit.dealership.jpa.maven.desktop.java.persistence;
 
+import com.halconbit.dealership.jpa.maven.desktop.java.logic.Automobile;
+
 /**
  *
  * @author Brayan Esteves
  */
 public class Persistence {
+
+    public void addAutomobile(Automobile automobile) {
+        
+    }
     
 }
