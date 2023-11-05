66f5964dc2a0120c98525fc29dedaa4738e6a024
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-05 19:50:27 -0400

    DTO - Add Entity "User" [05/11/2023]

--- src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/logic/User.java
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/logic/User.java
@@ -1,7 +1,16 @@
 package com.halconbit.dealership.jpa.maven.desktop.java.logic;
 
-public class User {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int    id;
     private String username;
     private String password;
