92be2b5978efa334dcb536bea546d328664a804e
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-10 18:55:16 -0400

    User with password not encrypted in data SQL of table "User" [10/11/2023]

---
+++ src/main/java/com/halconbit/dealership/jpa/maven/desktop/java/database/data/index.sql
@@ -0,0 +1,3 @@
+# User with password not encrypted
+INSERT INTO `dealership`.`USER` (`ID`, `PASSWORD`, `USERNAME`, `FK_ID_ROL`) VALUES(1, '1234', 'admin', 1);
+INSERT INTO `dealership`.`USER` (`ID`, `PASSWORD`, `USERNAME`, `FK_ID_ROL`) VALUES(2, '1234', 'user', 2);
